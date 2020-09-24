a3e7f682090ece112922e8abc7561f55c34a64cb
Marlon Emmerich <marlonemmerich@yahoo.com>
2020-09-24 16:58:15 -0300

    Insert check class of clicked element to prevent close dropdown

--- src/components/navbar/dropdownGeneric.ts
+++ src/components/navbar/dropdownGeneric.ts
@@ -3,6 +3,7 @@ import {DropDownColumn} from './dropdownColumn';
 
 export class DropDownGeneric extends NavbarElement {
     private _columns: Array<DropDownColumn>;
+    private _classesToPreventCloseOnClick: Array<String>;
     private _htmlElementContentSource: HTMLElement;
 
     get columns() {
@@ -21,16 +22,46 @@ export class DropDownGeneric extends NavbarElement {
         this._htmlElementContentSource = htmlElementContentSource;
     }
 
-    dropDownClick(dropDown: DropDownGeneric) : void {
-        const listClick = function(e: MouseEvent) {
-            if (!dropDown.htmlElementSource.contains((e.target as Element))) {
-                dropDown.htmlElementSource.classList.remove("openned");
-            }
-            document.removeEventListener('click', listClick, true);
+    get classesToPreventCloseOnClick() {
+        return this._classesToPreventCloseOnClick;
+    }
+
+    set classesToPreventCloseOnClick(classesToPreventCloseOnClick) {
+        this._classesToPreventCloseOnClick = classesToPreventCloseOnClick;
+    }
+
+    preventClose(event: MouseEvent) : boolean {
+        if(!this.classesToPreventCloseOnClick) {
+            return false;
         }
 
-        dropDown.htmlElementSource.onclick = function(event) {
-            const SCROLL_WIDTH = window.innerWidth - document.documentElement.clientWidth + 10;
+        console.log('event', event);
+
+        var arrayClassClickedElement = (event.target as Element).className.split(' ');
+
+        for(let indexArrayClassesClickedElmnt = 0; indexArrayClassesClickedElmnt < arrayClassClickedElement.length; indexArrayClassesClickedElmnt++) {
+            for(let indexArrayClasses = 0; indexArrayClasses < this.classesToPreventCloseOnClick.length; indexArrayClasses++) {
+                if(this.classesToPreventCloseOnClick[indexArrayClasses] === arrayClassClickedElement[indexArrayClassesClickedElmnt]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    dropDownClick(dropDown: DropDownGeneric) : void {
+        const listClick = (e: MouseEvent) => {
+            if(!this.preventClose(e)) {
+                if (!dropDown.htmlElementSource.contains((e.target as Element))) {
+                    dropDown.htmlElementSource.classList.remove("openned");
+                }
+                document.removeEventListener('click', listClick, true);
+            }
+        }
+
+        dropDown.htmlElementSource.onclick = (event) => {
+            let scrollWidth = window.innerWidth - document.documentElement.clientWidth + 10;
             let innerWidth = window.innerWidth;
 
             if(!dropDown.htmlElementSource.classList.contains('openned')) {
@@ -41,7 +72,7 @@ export class DropDownGeneric extends NavbarElement {
                 let position = dropDown.htmlElementContentSource.offsetLeft;
 
                 if(innerWidth < (dropDownContentWidth + position)) {
-                    let marginLeftToSet = (dropDownContentWidth + position + SCROLL_WIDTH) - innerWidth;
+                    let marginLeftToSet = (dropDownContentWidth + position + scrollWidth) - innerWidth;
 
                     dropDown.htmlElementContentSource.style.marginLeft = `-${marginLeftToSet}px`;
                 }
